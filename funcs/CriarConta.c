46d218481100cf88c5a8e1d65df2991b64cc31f4
limz12 <vascorololima2003@gmail.com>
2024-01-18 22:56:50 +0000

    adicionei a func de signup!
    Um simples fread, para ler o ficheiro linha a linha e comparar com o que o user introduziu.
    Se a pass e o user baterem certo, tem acesso à conta!

--- funcs/CriarConta.c
+++ funcs/CriarConta.c
@@ -1,11 +1,9 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 // Library where getch() is stored
 #include <conio.h>
 
-typedef struct contaBancaria
+typedef struct conta
 {
     char nome[25];
     char password[30];
@@ -13,10 +11,11 @@ typedef struct contaBancaria
 }CONTABANCARIA;
 
 char verificarConta[25];
+char veriricarPass[30];
 
 void signup()
 {
-    struct contaBancaria conta;
+    struct conta conta;
     FILE *fileptrA;
 
     printf("\nBem vindo ao registo do banco XPTO!");
@@ -40,30 +39,60 @@ void signup()
             //adicionar estas variaveis ao ficheiro
             fwrite(&conta, sizeof(conta), 1, fileptrA); //GUARDAR TUDO O QUE É DA CONTA
             printf("Conta criada com sucesso!");
-            printf("%s\t%s\t%i\n", conta.nome, conta.password, conta.montante);
             fclose(fileptrA);
         }else{
             printf("ERRO! Introduziste nomes diferentes!");
         }
     }
+
 }
  void leituraVerificar() //Verificar se a conta já existe no ficheiro se sim da exit do programa!
  {
-    CONTABANCARIA conta;
+    struct conta conta;
     FILE *fileptrB;
 
     fileptrB = fopen("files/contas.bin","rb");
-    
     while(fread(&conta, sizeof(CONTABANCARIA), 1, fileptrB)) // LE LINHA A LINHA E DA PRINT DO CONTEUDO
     { 
-        
         if(strcmp(conta.nome, verificarConta) == 0)
         {
-            printf("%s\t%s\t%i\n", conta.nome, conta.password, conta.montante);
             printf("ERRO! Esta conta ja existe!\n");
             fclose(fileptrB);
             exit(1); //E salta novamente para o signup() continuando da linha 36 para a frente
         }
     }
  }
+  
+  
+void login() //FUNCAO PARA DAR  LOGIN NA CONTA
+{  
+    struct conta conta; //para poder usar na func
+
     
+    printf("\nBem vindo ao sistema de Login do Banco XPTO!");
+    printf("\nInsira a nome da sua conta : ");
+    scanf("\n%s",&verificarConta);
+    printf("Insira a sua palavra passe : ");
+    scanf("\n%s",&veriricarPass);
+
+    //ABRIR O ficheiro para leitura
+    FILE *ptrfile;
+    ptrfile = fopen("files/contas.bin","rb");
+
+    //Ler o ficheiro à procura de um user igual com a mesma passe
+    while(fread(&conta,sizeof(CONTABANCARIA), 1, ptrfile)) //VAI LER O FICHEIRO LINHA A LINHA
+    {
+        if(strcmp(conta.nome, verificarConta) == 0)
+        {
+            if(strcmp(conta.password, veriricarPass) == 0)
+            {
+                printf("\nBem vindo à conta %s!",conta.nome);
+                printf("\nTens na conta : %i€",conta.montante);
+                fclose(ptrfile);
+            }
+        } else{
+            printf("\nERROR! Password ou Username errado!");
+        }
+    }
+    fclose(ptrfile);
+}  
--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "funcs/CriarConta.c"
+
 
 
 void main()
 {
     
-    signup();
+   //signup();
+    login();
     
 }
